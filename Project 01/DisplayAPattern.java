9baddc413ac192d496a7eef315cc8c66198be191
JsakoCS <104119408+JsakoCS@users.noreply.github.com>
2023-01-29 17:37:38 -0800

    DisplayAPattern.java

---
+++ Project 01/DisplayAPattern.java
@@ -0,0 +1,12 @@
+// Programmer : Jsako
+
+public class DisplayAPattern {
+    
+    public static void main(String[] args) {
+        System.out.println("    J      A     V     V     A");
+        System.out.println("    J    A   A    V   V    A   A");
+        System.out.println("J   J   AAAAAAA    V V    AAAAAAA");
+        System.out.println(" JJJ   A       A    V    A       A");
+    }
+    
+}
