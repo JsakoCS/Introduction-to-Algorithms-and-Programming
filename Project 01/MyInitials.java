2411199c18838b326d00fbf2d410af4c8f487fff
JsakoCS <104119408+JsakoCS@users.noreply.github.com>
2023-01-29 17:33:42 -0800

    MyInitials.java

---
+++ Project 01/MyInitials.java
@@ -0,0 +1,16 @@
+// Programmer : Jsako
+
+public class MyInitials {
+
+    public static void main(String[] args) {
+        System.out.println("       J   SSSSS");
+        System.out.println("       J  S     S");
+        System.out.println("       J   S");
+        System.out.println("       J    SSSSS");
+        System.out.println("J      J        S");
+        System.out.println("J      J  S      S");
+        System.out.println("J      J   S    S");
+        System.out.println(" JJJJJJ     SSSSS");
+    }
+    
+}
