e1e65b7b1081ce0b47674250b3bb8f8582a9b1da
JsakoCS <104119408+JsakoCS@users.noreply.github.com>
2023-01-31 20:49:17 -0800

    CostOfDriving.java

--- Project 02/CostOfDriving.java
+++ Project 02/CostOfDriving.java
@@ -1,1 +1,20 @@
+// Programmer : Jsako
 
+import java.util.Scanner;
+
+public class CostOfDriving {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter driving distance: ");
+        double drivingDistance = input.nextDouble();
+        System.out.print("Enter miles per gallon: ");
+        double milesGallon = input.nextDouble();
+        System.out.print("Enter price per gallon: ");
+        double priceGallon = input.nextDouble();
+        input.close();
+        double answer = (drivingDistance / milesGallon) * priceGallon;
+        System.out.printf("The cost of driving is $" + "%.2f", answer);
+    }
+    
+}
